5c59c4c0c773304e41e8ea8eb01ad8c0baa0181f
Tianyi Zhan <hpyzx666666@gmail.com>
2022-12-21 23:19:44 -0500

    add schemas forsanity

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,4 +1,10 @@
 import testimonials from "./testimonials";
+import works from './works';
+import brands from './brands';
+import abouts from './abouts';
+import experiences from './experiences';
+import skills from './skills';
+import workExperience from './workExperience';
+import contact from './contact';
 
-
-export const schemaTypes = [testimonials]
+export const schemaTypes = [testimonials, works, brands, abouts, experiences, skills,workExperience,contact] // put every schema types you created into the list
